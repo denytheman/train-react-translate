407237c341267fe8937baf2769bf42c4f8ad7289
Daniel Kovac <dkovac@logitech.com>
2019-03-15 13:46:20 -0700

    Initial project setup

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+    state = { language: 'english' }
+    render() {
+        return (
+            <div className="ui container">
+                <div>Select a language:
+                    <i className="flag us" onClick={() => {}}/>
+                    <i className="flag cs" />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
