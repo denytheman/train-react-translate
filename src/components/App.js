6246f477817928faaecd1f2afea64b3f3159ea92
Daniel Kovac <dkovac@logitech.com>
2019-03-15 14:19:27 -0700

    Setting the context to the new values

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import UserCreate from './UserCreate';
+import LanguageContext from '../context/LanguageContext';
 
 class App extends React.Component {
     state = { language: 'english' }
@@ -16,7 +17,9 @@ class App extends React.Component {
                     <i className="flag us" onClick={() => this.onLanguageChange('english')} />
                     <i className="flag cs" onClick={() => this.onLanguageChange('serbian')} />
                 </div>
-                <UserCreate />
+                <LanguageContext.Provider value={this.state.language}>
+                    <UserCreate />
+                </LanguageContext.Provider>
             </div>
         )
     }
